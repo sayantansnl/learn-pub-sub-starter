05319bea39736575bf1ef092c6401383c582baf4
sayantansnl <sayantansanyal72@gmail.com>
2026-03-03 12:18:02 +0530

    added basic repl commands

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -14,27 +14,48 @@ import (
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	gamelogic.PrintServerHelp()
 	connectionString := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("error in connecting to client, %v", err)
 	}
 	defer conn.Close()
-	// channel, err := conn.Channel()
-	// if err != nil {
-	// 	log.Fatalf("error in creating channel, %v", err)
-	// }
-	// err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-	// 	IsPaused: true,
-	// })
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("error in getting username: %v", err)
 	}
 	queueName := routing.PauseKey + "." + username
-	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+	channel, _, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
 	if err != nil {
 		log.Fatalf("failure to bind queue: %v", err)
+	}
+
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
+		first_word := userInput[0]
+		if first_word == "pause" {
+			fmt.Println("sending pause message")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+		} else if first_word == "resume" {
+			fmt.Println("sending resume message")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+		} else if first_word == "quit" {
+			fmt.Println("sending quit message, exiting")
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			break
+		} else {
+			fmt.Println("unable to understand command")
+		}
 	}
 
 	//wait for ctrl+c
