cb58f0ff620581bd4a74c1867d4cf4951a9c7d9c
sayantansnl <sayantansanyal72@gmail.com>
2026-03-03 09:36:10 +0530

    able to direct message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,17 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connection successful")
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error in creating channel, %v", err)
+	}
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("error in publishing with JSON: %v", err)
+	}
+
 	//wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
