477bbc0d051df53a1579642b09674d32b07b6ba2
sayantansnl <sayantansanyal72@gmail.com>
2026-03-03 11:06:21 +0530

    able to automatically add and remove queues

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("error in connecting to client, %v", err)
+	}
+	defer conn.Close()
+	// channel, err := conn.Channel()
+	// if err != nil {
+	// 	log.Fatalf("error in creating channel, %v", err)
+	// }
+	// err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+	// 	IsPaused: true,
+	// })
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("error in getting username: %v", err)
+	}
+	queueName := routing.PauseKey + "." + username
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatalf("failure to bind queue: %v", err)
+	}
+
+	//wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Connection closed.")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	jsonData, err := json.Marshal(val)
 	if err != nil {
@@ -16,4 +23,36 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		ContentType: "application/json",
 		Body:        jsonData,
 	})
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	durable := false
+	autoDelete := false
+	exclusive := false
+	if queueType == Durable {
+		durable = true
+	}
+	if queueType == Transient {
+		autoDelete = true
+		exclusive = true
+	}
+	queue, err := channel.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return channel, queue, nil
 }
